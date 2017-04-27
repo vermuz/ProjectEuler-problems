a18c34c887fa5cf64cd432445089ce3d5f68e7c6
Cory Koski <corykoski@gmail.com>
2017-04-27 15:41:19 -0400

    added new build results

---
+++ Problems/Problems/Problem11.js
@@ -0,0 +1,117 @@
+//// Problem 11
+//In the 20×20 grid below, four numbers along a diagonal line have been marked in red.
+//08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
+//49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
+//81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
+//52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
+//22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
+//24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
+//32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
+//67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
+//24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
+//21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
+//78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
+//16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
+//86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
+//19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
+//04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
+//88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
+//04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
+//20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
+//20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
+//01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48
+//The product of these numbers is 26 × 63 × 78 × 14 = 1788696.
+//What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20×20 grid?
+function Problem11() {
+    // get the grid in proper addressable format
+    function getRight(grid, x, y) {
+        var result = [];
+        for (var i = 0; i < sizeToGet; i++) {
+            result.push(grid[y][x + i]);
+        }
+        return result.reduce(function (a, b) { return a * b; });
+    }
+    function getDown(grid, x, y) {
+        var result = [];
+        for (var i = y; i < y + sizeToGet; i++) {
+            result.push(grid[i][x]);
+        }
+        return result.reduce(function (a, b) { return a * b; });
+    }
+    function getDiagNW2SE(grid, x, y) {
+        var result = [];
+        for (var i = 0; i < sizeToGet; i++) {
+            result.push(grid[y + i][x + i]);
+        }
+        return result.reduce(function (a, b) { return a * b; });
+    }
+    function getDiagSW2NE(grid, x, y) {
+        var result = [];
+        for (var i = 0; i < sizeToGet; i++) {
+            result.push(grid[y - i][x + i]);
+        }
+        return result.reduce(function (a, b) { return a * b; });
+    }
+    var gridStrings = [];
+    gridStrings.push("08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08");
+    gridStrings.push("49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00");
+    gridStrings.push("81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65");
+    gridStrings.push("52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91");
+    gridStrings.push("22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80");
+    gridStrings.push("24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50");
+    gridStrings.push("32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70");
+    gridStrings.push("67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21");
+    gridStrings.push("24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72");
+    gridStrings.push("21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95");
+    gridStrings.push("78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92");
+    gridStrings.push("16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57");
+    gridStrings.push("86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58");
+    gridStrings.push("19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40");
+    gridStrings.push("04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66");
+    gridStrings.push("88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69");
+    gridStrings.push("04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36");
+    gridStrings.push("20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16");
+    gridStrings.push("20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54");
+    gridStrings.push("01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48");
+    var grid = [];
+    var sizeToGet = 4;
+    for (var i = 0; i < gridStrings.length; i++) {
+        var numbers = gridStrings[i].split(" ");
+        var result = numbers.map(function (x) { return parseInt(x, 10); });
+        grid.push(result);
+    }
+    var yLastIndex = grid.length - 1;
+    var final = 0;
+    for (var y = 0; y < grid.length; y++) {
+        var xLastIndex = grid[y].length - 1;
+        for (var x = 0; x < grid[y].length; x++) {
+            var result = 0;
+            // right
+            if ((x + (sizeToGet - 1)) <= xLastIndex) {
+                result = getRight(grid, x, y);
+                if (result > final)
+                    final = result;
+            }
+            // down
+            if ((y + (sizeToGet - 1)) <= yLastIndex) {
+                result = getDown(grid, x, y);
+                if (result > final)
+                    final = result;
+            }
+            //// NWSE
+            if ((x - (sizeToGet - 1)) >= 0 && (y + (sizeToGet - 1)) <= yLastIndex) {
+                result = getDiagNW2SE(grid, x, y);
+                if (result > final)
+                    final = result;
+            }
+            //// SWNE
+            if ((x + (sizeToGet - 1)) <= xLastIndex && (y - (sizeToGet - 1)) >= 0) {
+                result = getDiagSW2NE(grid, x, y);
+                if (result > final)
+                    final = result;
+            }
+        }
+    }
+    console.log(final);
+}
+//# sourceMappingURL=Problem11.js.map
--- Problems/Problems/Problem6.js
+++ Problems/Problems/Problem6.js
@@ -5,16 +5,16 @@
 //    (1 + 2 + ... + 10)2 = 552 = 3025
 //Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
 //Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+function add(a, b) {
+    return a + b;
+}
+function addSquares(a, b) {
+    return a + Math.pow(b, 2);
+}
 function Problem6() {
     var _ = require("underscore");
     var bottomNum = 1;
     var topNum = 100;
-    function add(a, b) {
-        return a + b;
-    }
-    function addSquares(a, b) {
-        return a + Math.pow(b, 2);
-    }
     var numbers = _.range(bottomNum, topNum + 1);
     var sumSquared = Math.pow(numbers.reduce(add, 0), 2);
     var sumOfSquares = numbers.reduce(addSquares, 0);
